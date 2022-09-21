46bc40465be0e68b4857569e83e8507efe7dffd3
Karina <karina.dragan4uk@yandex.ru>
2022-09-21 18:04:10 +0300

    Задача с конфетами решена!

--- home_work_5.py
+++ home_work_5.py
@@ -28,6 +28,57 @@
 
 # b) Подумайте как наделить бота ""интеллектом""
 
+from msilib import sequence
+import random
+
+def who_is_1(name1: str, name2: str):
+    if random.randint(0,1):
+        player1 = name1
+        player2 = name2
+    else:
+        player1 = name2
+        player2 = name1
+    return player1, player2
+
+def victory(players: tuple, first_move: int, k=2021, step=28):
+    whose_move = players[0]
+
+    k -= first_move
+    while k > 0:
+        k -= random.randint(1, step)
+        if whose_move == players[0]:
+            whose_move = players[1]
+        else:
+            whose_move = players[0]
+
+    return f'Побеждает {whose_move}!'
+
+
+
+def hint_for_player1(k=2021, step=28):
+    for i in range(1, step+1):
+        if not (k - i) % (step + 1):
+            return i
+
+
+print('Введите свои имена: ')
+name1, name2 = input(), input()
+order_move = who_is_1(name1, name2)
+starter = order_move[0]
+second = order_move[1]
+
+print()
+
+print(f'{starter} - ты начинаешь игру')
+print(f'Подсказка: {starter}, чтобы победить, возьми на первом ходу {hint_for_player1()} конфет')
+
+
+step_player1 = int(input('Твой ход: '))
+
+print()
+
+print(victory(order_move, step_player1))
+
 
 
 
@@ -39,3 +90,67 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from tkinter import *
+
+# window = Tk()
+# label = Label(
+#     text='Начинаем игру!',
+#     foreground='saddle brown',
+#     background='peach puff',
+#     width=20,
+#     height=10
+#     )
+
+# label.pack()
+# window.mainloop()
+
+
+# button = Button(
+#     text='Начинаем игру!',
+#     foreground='saddle brown',
+#     background='peach puff',
+#     width=20,
+#     height=10
+#     )
+
+# button.pack()
+# window.mainloop()
+
+
+# label = Label(text='Сколько конфет вы возьмете?')
+# entry = Entry()
+# count = entry.get()
+
+# label.pack()
+# entry.pack()
+# window.mainloop()
