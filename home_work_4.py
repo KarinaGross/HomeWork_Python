7a03f42f6a4bcd057fce964462dbc62ec0fe0182
Karina <karina.dragan4uk@yandex.ru>
2022-10-01 18:20:02 +0300

    Задача 5 решена

--- home_work_4.py
+++ home_work_4.py
@@ -149,157 +149,65 @@
 
 
 
+
 def get_polinom(file_name: str) -> list:
     with open(f'{file_name}') as file:
         while True:
             line = file.readline()
             if not line:
                 break
-            result = line.split()
-        
-    # for i in range(len(result)):
-    #     result[i] = result[i].split('*')
-
-    # result = sum(result, [])
+            result = line
     return result
     
+
+def write_to_file(name_file: str, text: str):
+    with open(f'{name_file}', 'a') as file:
+        file.write(f'{text}\n')
+
+
+def sum_polinom(lst_pol1: str, lst_pol2: str) -> str:
+    pol1 = lst_pol1[:-4].split(' + ')
+    pol2 = lst_pol2[:-4].split(' + ')
+    general_list = pol1 + pol2
+    general_list = [item.split('*') for item in general_list]
+    for i in range(len(pol1)):
+        for j in range(len(pol1), len(general_list)):
+            # if general_list[i].isdigit() and general_list[j].isdigit():
+            #     general_list[i] = f'{int(general_list[i]) + int(general_list[j])}'
+            #     general_list[j] = 0
+            if len(general_list[i]) == len(general_list[j]) == 2:
+                if general_list[i][1] == general_list[j][1]:
+                    sum_coef = int(general_list[i][0]) + int(general_list[j][0])
+                    # result.append(f'{sum_coef}*{general_list[i][1]}')
+                    general_list[i] = f'{sum_coef}*{general_list[i][1]}'
+                    general_list[j] = ''
+                    
+
+            elif len(general_list[i]) == 1:
+                if len(general_list[j]) == 1:
+                    elem1 = int(general_list[i][0])
+                    elem2 = int(general_list[j][0])
+                    general_list[i] = f'{elem1 + elem2}'
+                    general_list[j] = ''
+
+
+    result = []
+    for i in range(len(general_list)):
+        if type(general_list[i]) == list:
+            result.append('*'.join(general_list[i]))
+        elif general_list[i] != '':
+            result.append(general_list[i])
+    
+    result = ' + '.join(result) + ' = 0'
+
+
+    return result
+
+
 pol1 = get_polinom('polinom_4.5.1.txt')
 pol2 = get_polinom('polinom_4.5.2.txt')
-print(pol1)
-print(pol2)
-
-# def sum_polinom(lst_pol1: list, lst_pol2: list) -> list:
-#     lst_pol1.reverse()
-#     lst_pol2.reverse()
-#     result = []
-#     max_len = max(len(lst_pol1), len(lst_pol2))
-#     for i in range(max_len):
-#         if i == 1:
-#         result.append(int(lst_pol1[i]) + int(lst_pol2[i]))
-
-#         if 
-    
-#     result.reverse()
-#     return result
-        
-# print(sum_polinom(pol1, pol2))
-
-# def couple_elements(lst_pol1: list, lst_pol2: list) -> list:
-#     result = []
-#     for i in range(len(lst_pol1)):
-#         for j in range(len(lst_pol2)):
-#             if lst_pol1[i + 1] == lst_pol2[i + 1]:
-#                 if lst_pol1[i].isdigit() and lst_pol2.isdigit():
-#                     sum_coef = int(lst_pol1[i]) + int(lst_pol2[i])
-#                     result.append(f'{sum_coef}*{lst_pol1[i + 1]}')
-#                 else:
 
 
-# def sum_polinom(lst_pol1: list, lst_pol2: list) -> list:
-#     result = []
-#     for i in range(len(lst_pol1)):
-#         for j in range(len(lst_pol2)):
-#             if lst_pol1[i][-1] == lst_pol2[j][-1]:
-#                 if lst_pol1[i][-1].isdigit() and lst_pol2[j][-1].isdigit():
-#                     lst_pol1[i] = lst_pol1[i].split('*')
-#                     lst_pol2[j] = lst_pol2[j].split('*')
-#                     sum_coef = int(lst_pol1[i][0]) + int(lst_pol2[j][0])
-#                     result.append(f'{sum_coef}*{lst_pol1[i][1]}')
-#                     break
-#                 else:
-#                     result.append(lst_pol1[i])
-#                     break
+write_to_file('sum.txt', sum_polinom(pol1, pol2))
             
-#             elif lst_pol1[i] == lst_pol2[j] == '+':
-#                 result.append(lst_pol1[i])
-#                 break
-#     return result
-
-
-def sum_polinom(lst_pol1: list, lst_pol2: list) -> list:
-    general_list = lst_pol1 + lst_pol2
-    result = lst_pol1
-    for i in range(len(lst_pol1)):
-        for j in range(len(lst_pol2)):
-            if result[i] == lst_pol2[j]:
-                # if not general_list[i].isalnum():
-                #     del general_list[j]
-
-                if result[i].isdigit():
-                    result[i] = str(int(result[i]) + int(lst_pol2[j]))
-                    # del general_list[j]
-
-            elif result[i][-1] == lst_pol2[j][-1]:
-                if not result[i][-1].isdigit():
-                    elem_i = result[i].split('*')
-                    elem_j = lst_pol2[j].split('*')
-                    sum_coef = int(elem_i[0]) + int(elem_j[0])
-                    result[i] = f'{sum_coef}*{elem_i[1]}'
-                # del general_list[j]
-
-    return result
-
-print(sum_polinom(pol1, pol2))
-                
-            
-
-
-
-# Даны два файла, в каждом из которых находится запись многочлена. 
-# Задача - сформировать файл, содержащий сумму многочленов.
-
-# def sum_of_polynimials(polynomials_1: list[str], polynomials_2: list[str]) ->list:
-
-# def getting_list_functions(file_name: str) ->list:
-    
-#     with open(f'Homework/Seminar_4/005_sum_of_polynomials/{file_name}', 'r') as file:
-#         polynomials = file.read().split('\n')
-#     for i in range(len(polynomials)):
-#         polynomials[i] = polynomials[i].replace(' = 0', '').split(' + ')
-#         for k in range(len(polynomials[i])):
-#             polynomials[i][k] = polynomials[i][k].split('*')
-    
-#     return polynomials
-
-# def write_to_file(name_file: str, text: str):
-#     with open(f'Homework/Seminar_4/005_sum_of_polynomials/{name_file}', 'a') as file:
-#         file.write(f'{text}\n')
-
-# def sum_polynomials(polynomials_1: list, polynomials_2: list) -> str:
-
-#     polynomials_max_len = polynomials_1
-#     polynomials_min_len = polynomials_2
-#     if polynomials_1 < polynomials_2:
-#         polynomials_max_len = polynomials_2
-#         polynomials_min_len = polynomials_1
-
-#     result= ''
-#     for i in range(len(polynomials_max_len)):
-#         if len(polynomials_max_len[i]) != 1:
-#             for k in range(len(polynomials_min_len)):
-#                 if len(polynomials_min_len[k]) != 1:
-#                     if  polynomials_max_len[i][1] == polynomials_min_len[k][1]:
-#                         result += f"{int(polynomials_max_len[i][0]) + int(polynomials_min_len[k][0])}*{polynomials_max_len[i][1]}"
-#                         break
-#                 elif (k == len(polynomials_min_len) - 1):
-#                     result += f'{polynomials_max_len[i][0]}*{polynomials_max_len[i][1]}'
-#         else:
-#             for k in range(len(polynomials_min_len)):
-#                 if len(polynomials_min_len[k]) == 1:
-#                     result += f'{int(polynomials_max_len[i][0])+ int(polynomials_min_len[k][0])}'
-            
-#         if i != (len(polynomials_max_len) -1):
-#                 result += ' + ' 
-#     return result + ' = 0'
-
-
-# polynomials_1 = getting_list_functions('polynomial_1.txt')
-# polynomials_2 = getting_list_functions('polynomial_2.txt')
-
-# new_polynomials = []
-# for i in range(len(polynomials_1)):
-#     new_polynomials.append(sum_polynomials(polynomials_1[i], polynomials_2[i]))
-
-# for i in new_polynomials:
-#     write_to_file('Sum.txt', i)
 
