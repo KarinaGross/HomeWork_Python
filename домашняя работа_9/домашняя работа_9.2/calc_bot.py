f085b491404460d4aa4f4ce8d2c76d83b701fc6b
KarinaGross <107581848+KarinaGross@users.noreply.github.com>
2022-10-07 16:56:09 +0300

    Update and rename home_work_9/home_work_9.2/calc_bot.py to домашняя работа_9/домашняя работа_9.2/calc_bot.py
    Токен заменен на слово "TOKEN"

--- домашняя работа_9/домашняя работа_9.2/calc_bot.py
+++ домашняя работа_9/домашняя работа_9.2/calc_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from calc_bot_commands import *
 
 
-app = ApplicationBuilder().token("5662048036:AAFfSqSOkNhf_NdM11a36gfQfjwep_YGs-o").build()
+app = ApplicationBuilder().token("TOKEN").build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("bye", bye))
