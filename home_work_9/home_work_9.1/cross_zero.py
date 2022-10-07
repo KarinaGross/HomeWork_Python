1b0353892caac92716cb85a24ea9faa2b69f9956
Karina <karina.dragan4uk@yandex.ru>
2022-10-07 16:45:38 +0300

    Крестики-нолики с библиотекой pygame

--- home_work_9/home_work_9.1/cross_zero.py
+++ home_work_9/home_work_9.1/cross_zero.py
@@ -1,90 +1,93 @@
-from tkinter import * 
-from tkinter import messagebox
- 
-Player1 = 'X'
-stop_game = False
- 
-def clicked(r,c):
-     
-    global Player1
-    
- 
-    if Player1 == "X" and states[r][c] == 0 and stop_game == False:
-        button[r][c].configure(text = "X")
-        states[r][c] = 'X'
-        Player1='O'
- 
-     
-    if Player1 == 'O' and states[r][c] == 0 and stop_game == False:
-        button[r][c].configure(text = 'O')
-        states[r][c] = "O"
-        Player1 = "X"
- 
-    check_if_win()
+import pygame
+import sys
 
+def check_win(mas, sign):
+    zeroes = 0
+    for row in mas:
+        zeroes += row.count(0)
+        if row.count(sign) == 3:
+            return sign
+    for col in range(3):
+        if mas[0][col] == sign and mas[1][col] == sign and mas[2][col] == sign:
+            return sign
+    if mas[0][0] == sign and mas[1][1] == sign and mas[2][2] == sign:
+            return sign
+    if mas[0][2] == sign and mas[1][1] == sign and mas[2][0] == sign:
+            return sign
+    if zeroes == 0:
+        return 'Piece!'
 
-def check_if_win():
-    global stop_game
- 
-    for i in range(3):
-        if states[i][0] == states[i][1] == states[i][2] !=0:
-            stop_game = True
-    
-            messagebox.showinfo("Winner", f"'{states[i][0]}' победили!")
-            break
-    
-        elif states [0][i] == states[1][i] == states[2][i] != 0:
-            stop_game = True
-    
-            messagebox.showinfo("Winner", f"'{states[0][i]}' победили!")
-            break
-    
-        elif states[0][0] == states[1][1] == states[2][2] !=0:
-            stop_game = True
-    
-            messagebox.showinfo("Winner", f"'{states[0][0]}' победили!")
-            break
-    
-        elif states[0][2] == states[1][1] == states[2][0] !=0:
-            stop_game = True
-    
-            messagebox.showinfo("Winner" , f"'{states[0][2]}' победили!")
-            break
-    
-        elif states[0][0] and states[0][1] and states[0][2] and states[1][0] and states[1][1] and states[1][2] and states[2][0] and states[2][1] and states[2][2] != 0:
-            stop_game = True
-    
-            messagebox.showinfo("tie", "Ничья!")
-        
+pygame.init()
 
+size_block = 100
+margin = 15
+width = height = size_block*3 + margin*4
 
-window = Tk()           
-window.title('Крестики-нолики') 
-window.resizable(0,0)
- 
-#Button
-button = [
-     [0,0,0],
-     [0,0,0],
-     [0,0,0]]
- 
-#text for buttons
-states = [
-     [0,0,0],
-     [0,0,0],
-     [0,0,0]]
- 
-for i in range(3):
-    for j in range(3):
-                                          
-        button[i][j] = Button(
-                        height = 4, width = 8,
-                        font = ("Helvetica","20"),
-                        bg='SkyBlue1',
-                        command = lambda r = i, c = j : clicked(r,c))
-        button[i][j].grid(row = i, column = j)
- 
- 
-mainloop()        
+size_window = (width, height)
+screen = pygame.display.set_mode(size_window)
+pygame.display.set_caption("Крестики-нолики")
 
+red = (225, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+black = (0, 0, 0)
+mas = [[0]*3 for i in range(3)]
+query = 0 # 1 2 3 4 5 6 7 
+game_over = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.MOUSEBUTTONDOWN and not game_over:
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            # print(f'x = {x_mouse}, y = {y_mouse}')
+            col = x_mouse // (size_block + margin)
+            row = y_mouse // (size_block + margin)
+            if mas[row][col] == 0:
+                if query % 2 == 0:
+                    mas[row][col] = 'x'
+                else:
+                    mas[row][col] = 'o'
+                query += 1
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            game_over = False
+            mas = [[0]*3 for i in range(3)]
+            query = 0
+            screen.fill(black)
+
+    if not game_over:
+        for row in range(3):
+            for col in range(3):
+                if mas[row][col] == 'x':
+                    color = red
+                elif mas[row][col] == 'o':
+                    color = green
+                else: 
+                    color = white
+                x = col * size_block + margin * (col+1)
+                y = row * size_block + margin * (row + 1)
+                pygame.draw.rect(screen, color, (x,y,size_block,size_block))
+                if color == red:
+                    pygame.draw.line(screen, white, (x+5, y+5), (x + size_block - 5, y + size_block - 5), 3)
+                    pygame.draw.line(screen, white, (x + size_block - 5, y+5), (x + 5, y + size_block - 5), 3)
+                elif color == green:
+                    pygame.draw.circle(screen, white, (x + size_block//2, y + size_block//2), size_block//2 - 3, 3)
+
+    if (query-1) % 2 == 0: # x - четные
+        game_over = check_win(mas, 'x')
+    else:
+        game_over = check_win(mas, 'o')
+    
+    if game_over:
+        screen.fill(black)
+        font = pygame.font.SysFont('stxingkai', 80)
+        text1 = font.render(game_over, True, white)
+        text_rect = text1.get_rect()
+        text_x = screen.get_width() / 2 - text_rect.width / 2
+        text_y = screen.get_height() / 2 - text_rect.height / 2
+        screen.blit(text1, [text_x, text_y])
+
+    pygame.display.update()
 
